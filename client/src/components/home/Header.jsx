9b12e55cf4246159eb73a7eb10f5673e42e49571
Mason Hirst <mason.j.hirst@gmail.com>
2026-02-09 17:29:18 -0700

    updating the pipeline to include pushing to fly.io

--- client/src/components/home/Header.jsx
+++ client/src/components/home/Header.jsx
@@ -57,7 +57,7 @@ const Header = ({hasScroll}) => {
             onClick={() => navigate('/')}
             sx={{ fontSize: '15px', opacity: 0.6 }}
           >
-            by Mason Hirst broski
+            by Mason Hirst licious
           </Typography>
         </Box>
       </Box>
